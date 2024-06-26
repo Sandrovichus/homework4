26e083fe5bdca0e5588cce071e9278c544b2c2f9
Alexander <veber21@rambler.ru>
2024-06-26 09:52:06 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input('Введите текст: ')
+
+print('Количество символов:', len(my_string))
+
+print(my_string.upper())    # текст в верхнем регистре
+print(my_string.lower())    # текст в нижнем регистре
+print(my_string.replace(' ', ''))    # удаление пробелов в тексте
+print(my_string[0])    # первый символ текста
+print(my_string[-1])    # последний символ текста
